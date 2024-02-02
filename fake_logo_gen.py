03bf51e90b9480da83ed883116f5aefb097f6880
Soumyojyoti Saha <110711548+Soumyojyotisaha@users.noreply.github.com>
2024-02-02 23:25:50 +0530

    Update fake_logo_gen.py

--- fake_logo_gen.py
+++ fake_logo_gen.py
@@ -2,22 +2,25 @@ import os
 import cv2
 import pandas as pd
 
+# Set input and output folders, and transformation parameters
 logo_folder = 'output'
 output = 'genLogoOutput'
-
 scale_factor = 0.8
 rotation_angle = 30
 
+# Data list to store information about the transformed images
 data = []
+
+# Iterate through the logo images in the specified folder
 for root, dirs, files in os.walk(logo_folder):
     for filename in files:
-        if filename.endswith('.jpg') or filename.endswith('.png'):  # Adjust the file extensions based on your logo image format
+        if filename.endswith('.jpg') or filename.endswith('.png'):  # Adjust file extensions based on your logo image format
             logo_path = os.path.join(root, filename)
             brand_name = root.replace('output\\', '')
 
             brand_folder = os.path.join(output, brand_name)
-            if not os.path.exists(os.path.join(output, brand_name)):
-                os.makedirs(os.path.join(output, brand_name))
+            if not os.path.exists(brand_folder):
+                os.makedirs(brand_folder)
             
             # Load the logo image
             image = cv2.imread(logo_path)
@@ -27,20 +30,22 @@ for root, dirs, files in os.walk(logo_folder):
             rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), rotation_angle, 1)
             rotated_image = cv2.warpAffine(image, rotation_matrix, (cols, rows))
             
-            # Save the transformed image
+            # Save the rotated image
             cv2.imwrite(os.path.join(brand_folder, filename), rotated_image)
             
+            # Update data list with information about the rotated image
             data.append({'Filename': os.path.join(brand_folder, filename), 'Brand Name': brand_name, 'Label': 'Fake'})
 
+# Iterate through the logo images again to apply scaling transformation
 for root, dirs, files in os.walk(logo_folder):
     for filename in files:
-        if filename.endswith('.jpg') or filename.endswith('.png'):  # Adjust the file extensions based on your logo image format
+        if filename.endswith('.jpg') or filename.endswith('.png'):  # Adjust file extensions based on your logo image format
             logo_path = os.path.join(root, filename)
             brand_name = root.replace('output\\', '')
 
             brand_folder = os.path.join(output, brand_name)
-            if not os.path.exists(os.path.join(output, brand_name)):
-                os.makedirs(os.path.join(output, brand_name))
+            if not os.path.exists(brand_folder):
+                os.makedirs(brand_folder)
             
             # Load the logo image
             image = cv2.imread(logo_path)
@@ -48,13 +53,19 @@ for root, dirs, files in os.walk(logo_folder):
             # Apply scaling transformation
             scaled_image = cv2.resize(image, None, fx=scale_factor, fy=scale_factor)
             
-            # Save the transformed images
+            # Save the scaled image
             cv2.imwrite(os.path.join(brand_folder, 'scal_'+filename), scaled_image)
             
+            # Update data list with information about the scaled image
             data.append({'Filename': os.path.join(brand_folder, 'scal_'+filename), 'Brand Name': brand_name, 'Label': 'Fake'})
 
+# Read the existing CSV file containing original images information
 df = pd.read_csv('file_map.csv')
+
+# Concatenate the existing DataFrame with the new data
 n_df = pd.concat([df, pd.DataFrame(data)], axis=0)
+
+# Save the updated DataFrame to a new CSV file
 n_df.to_csv('file_mapping.csv', index=False)
 
 print("Done")
